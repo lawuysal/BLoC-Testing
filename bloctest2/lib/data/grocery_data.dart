a3a9d3fc2b743b021d23bdf10b1a3e5957013ed1
Oğuzhan Uysal <law-tekken3@hotmail.com>
2023-09-03 21:33:24 +0300

    New Project

---
+++ bloctest2/lib/data/grocery_data.dart
@@ -0,0 +1,64 @@
+class GroceryData {
+  static List<Map<String, dynamic>> groceryProducts = [
+    {
+      "id": 1,
+      "name": "Apple",
+      "description": "Fresh and juicy red apple",
+      "price": 1.0,
+      "imageUrl": "https://5.imimg.com/data5/AK/RA/MY-68428614/apple.jpg"
+    },
+    {
+      "id": 2,
+      "name": "Banana",
+      "description": "Ripe and sweet yellow banana",
+      "price": 0.75,
+      "imageUrl":
+          "https://m.media-amazon.com/images/I/51ebZJ+DR4L._AC_UF1000,1000_QL80_.jpg"
+    },
+    {
+      "id": 3,
+      "name": "Orange",
+      "description": "Tangy and refreshing orange",
+      "price": 1.25,
+      "imageUrl":
+          "https://img.theculturetrip.com/1200x675/wp-content/uploads/2018/03/origins-of-the-word-orange.jpg"
+    },
+    {
+      "id": 4,
+      "name": "Strawberry",
+      "description": "Plump and delicious strawberries",
+      "price": 2.5,
+      "imageUrl": "https://www.georgeperry.co.uk/images/P/strawberries.jpg"
+    },
+    {
+      "id": 5,
+      "name": "Carrot",
+      "description": "Crunchy and nutritious carrots",
+      "price": 0.5,
+      "imageUrl": "https://safwachemicals.com/wp-content/uploads/2019/12/p9.jpg"
+    },
+    {
+      "id": 6,
+      "name": "Lettuce",
+      "description": "Fresh and crisp lettuce leaves",
+      "price": 1.0,
+      "imageUrl":
+          "https://cdn.britannica.com/77/170677-050-F7333D51/lettuce.jpg"
+    },
+    {
+      "id": 7,
+      "name": "Tomato",
+      "description": "Ripe and juicy tomatoes",
+      "price": 1.0,
+      "imageUrl": "https://www.mygos.com.tr/wp-content/uploads/2019/12/p3.jpg"
+    },
+    {
+      "id": 8,
+      "name": "Grapes",
+      "description": "Sweet and seedless grapes",
+      "price": 2.0,
+      "imageUrl":
+          "https://masseysofsaintfield.co.uk/wp-content/uploads/2020/10/green-grapes.jpg"
+    }
+  ];
+}
