8ff7e895e336ecc8eb857b73548f1802af57d3c3
Oğuzhan Uysal <law-tekken3@hotmail.com>
2023-10-28 22:23:52 +0300

    Added tests for [AuthBloc, GetUsersEvent]

--- cleanarchtdd/test/src/authentication/presentation/bloc/authentication_bloc_test.dart
+++ cleanarchtdd/test/src/authentication/presentation/bloc/authentication_bloc_test.dart
@@ -1,4 +1,5 @@
 import 'package:bloc_test/bloc_test.dart';
+import 'package:cleanarchtdd/core/errors/failure.dart';
 import 'package:cleanarchtdd/src/authentication/domain/usecases/create_user.dart';
 import 'package:cleanarchtdd/src/authentication/domain/usecases/get_users.dart';
 import 'package:cleanarchtdd/src/authentication/presentation/bloc/authentication_bloc.dart';
@@ -15,6 +16,7 @@ void main() {
   late GetUsers getUsers;
   late AuthenticationBloc bloc;
   const testCreateUserParams = CreateUserParams.empty();
+  const testApiFailure = APIFailure(message: 'Server Error', statusCode: 400);
 
   setUp(() {
     createUser = MockCreateUser();
@@ -31,21 +33,82 @@ void main() {
 
   group('createUser', () {
     blocTest<AuthenticationBloc, AuthenticationState>(
-        'Should emit [CreatingUserState, UserCreatedState] when successful',
+      'Should emit [CreatingUserState, UserCreatedState] when successful',
+      build: () {
+        when(() => createUser(testCreateUserParams))
+            .thenAnswer((_) async => const Right(null));
+        return bloc;
+      },
+      act: (bloc) => bloc.add(CreateUserEvent(
+          createdAt: testCreateUserParams.createdAt,
+          name: testCreateUserParams.name,
+          avatar: testCreateUserParams.avatar)),
+      expect: () => const [CreatingUserState(), UserCreatedState()],
+      verify: (bloc) {
+        verify(() => createUser(testCreateUserParams)).called(1);
+
+        verifyNoMoreInteractions(createUser);
+      },
+    );
+
+    blocTest(
+      'Should emit [CreatingUserState, AuthenticationErrorState] when failed ',
+      build: () {
+        when(
+          () => createUser(any()),
+        ).thenAnswer((_) async => const Left(testApiFailure));
+        return bloc;
+      },
+      act: (bloc) => bloc.add(
+        CreateUserEvent(
+          createdAt: testCreateUserParams.createdAt,
+          name: testCreateUserParams.name,
+          avatar: testCreateUserParams.avatar,
+        ),
+      ),
+      expect: () => [
+        const CreatingUserState(),
+        AuthenticationErrorState(testApiFailure.errorMessage)
+      ],
+      verify: (bloc) {
+        verify(() => createUser(testCreateUserParams)).called(1);
+        verifyNoMoreInteractions(createUser);
+      },
+    );
+  });
+  group(
+    'getUsers',
+    () {
+      blocTest<AuthenticationBloc, AuthenticationState>(
+          'Should emit [GettingUsersState, UsersLoadedState] when successful',
+          build: () {
+            when(() => getUsers()).thenAnswer((_) async => const Right([]));
+            return bloc;
+          },
+          act: (bloc) => bloc.add(const GetUsersEvent()),
+          expect: () => const [GettingUsersState(), UsersLoadedState([])],
+          verify: (_) {
+            verify(() => getUsers()).called(1);
+            verifyNoMoreInteractions(getUsers);
+          });
+
+      blocTest(
+        'Should emit [GettingUsersState, AuthenticationErrorState]',
         build: () {
-          when(() => createUser(testCreateUserParams))
-              .thenAnswer((_) async => const Right(null));
+          when(() => getUsers())
+              .thenAnswer((_) async => const Left(testApiFailure));
           return bloc;
         },
-        act: (bloc) => bloc.add(CreateUserEvent(
-            createdAt: testCreateUserParams.createdAt,
-            name: testCreateUserParams.name,
-            avatar: testCreateUserParams.avatar)),
-        expect: () => const [CreatingUserState(), UserCreatedState()],
+        act: (bloc) => bloc.add(const GetUsersEvent()),
+        expect: () => [
+          const GettingUsersState(),
+          AuthenticationErrorState(testApiFailure.errorMessage)
+        ],
         verify: (bloc) {
-          verify(() => createUser(testCreateUserParams)).called(1);
-
-          verifyNoMoreInteractions(createUser);
-        });
-  });
+          verify(() => getUsers()).called(1);
+          verifyNoMoreInteractions(getUsers);
+        },
+      );
+    },
+  );
 }
